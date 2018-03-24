27706ad6aa3f47a99dd732791e6e675f512d7d12
John Bergdahl <john@bergdahl.co>
2018-03-24 22:36:17 +0100

    Browser test

---
+++ tests/header.test.js
@@ -0,0 +1,42 @@
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await page.close();
+});
+
+test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+    expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts the OAuth flow', async () => {
+    await page.click('.right a');
+
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in doesnt display the login button', async () => {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await page.setCookie({ name: 'session', value: session });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
+
+});
