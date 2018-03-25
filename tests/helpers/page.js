cc2e6c82d7b85a45c48b0996e4a002ea541f91f2
John Bergdahl <john@bergdahl.co>
2018-03-25 18:18:17 +0200

    Refactor tests. Add blogs tests.

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('../factories/sessionFactory');
+const userFactory = require('../factories/userFactory');
+
 
 class CustomPage {
     static async build() {
@@ -19,6 +22,53 @@ class CustomPage {
     constructor(page) {
         this.page = page;
     }
+
+    async login() {
+        const user = await userFactory();
+        const { session, sig } = sessionFactory(user);
+
+        await this.page.setCookie({ name: 'session', value: session });
+        await this.page.setCookie({ name: 'session.sig', value: sig });
+        await this.page.goto('localhost:3000/blogs');
+        await this.page.waitFor('a[href="/auth/logout"]');
+    }
+
+    async getContentsOf(selector) {
+        return this.page.$eval(selector, el => el.innerHTML);
+    }
+
+    get(path) {
+        return this.evaluate((_path) => {
+            return fetch(_path, {
+                method: 'GET',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json());
+        }, path);
+    }
+
+    post(path, data) {
+        return this.evaluate((_path, _data) => {
+                return fetch(_path, {
+                    method: 'POST',
+                    credentials: 'same-origin',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(_data)
+                }).then(res => res.json());
+        }, path, data);
+
+    }
+
+    execRequests(actions) {
+        return Promise.all(actions.map(({ method, path, data }) => {
+            return this[method](path, data);
+        }));
+    }
+
 }
 
 module.exports = CustomPage;
