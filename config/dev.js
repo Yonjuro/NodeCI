f7a0941835ebe20d6885f2194d19b533716119f6
John Bergdahl <john@bergdahl.co>
2018-03-26 08:46:35 +0200

    Upload to S3

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://john:password0@ds119059.mlab.com:19059/blog_dev',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
